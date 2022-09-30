03f2228678b5c2145eaa839b227e7934d6ddf3b7
didwogur28 <didwogur28@naver.com>
2022-09-30 21:48:21 +0900

    feat: 상태코드 관리 개발

---
+++ src/main/java/baseball/StatusCode.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class StatusCode {
+
+    public String NOT_INTEGER_TYPE = "올바른 숫자가 아닙니다. 세자리 숫자를 입력해주세요.";
+    public String ARRAY_SIZE_ERROR = "세자리의 숫자를 입력해주세요.";
+    public String ARRAY_CONTAIN_ERROR = "0을 제외한 숫자를 입력해주세요.";
+    public String ARRAY_DUP_ERROR = "중복된 숫자는 입력하실수 없습니다.";
+}
