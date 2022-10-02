71cfe3605d4bdc91ac49a08f570c300387930644
didwogur28 <didwogur28@naver.com>
2022-10-02 17:29:39 +0900

    feat: 종료 시점 메시지 및 코드 추가

--- src/main/java/baseball/StatusCode.java
+++ src/main/java/baseball/StatusCode.java
@@ -6,4 +6,8 @@ public class StatusCode {
     public String ARRAY_SIZE_ERROR = "세자리의 숫자를 입력해주세요.";
     public String ARRAY_CONTAIN_ERROR = "0을 제외한 숫자를 입력해주세요.";
     public String ARRAY_DUP_ERROR = "중복된 숫자는 입력하실수 없습니다.";
+    public String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public String GAME_RESTART = "게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.";
+
+    public String GAME_RESTART_ERROR = "올바른 숫자가 아닙니다. 1 또는 2를 입력하세요.";
 }
