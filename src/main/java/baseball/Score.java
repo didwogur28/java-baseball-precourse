b9e5d803093a68419f63caa1d3ff1891c0fbbef1
didwogur28 <didwogur28@naver.com>
2022-10-03 11:43:04 +0900

    refactor: 볼 스트라이크 콤마 제거

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -7,7 +7,7 @@ public class Score {
     public String scoreReturn(int ball, int strike) {
 
         if (ball != 0 && strike != 0) {
-            return (ball + "볼" + " , " + strike + "스트라이크");
+            return (ball + "볼" + " " + strike + "스트라이크");
         }
         if (ball == 0 && strike != 0) {
             return (strike + "스트라이크");
